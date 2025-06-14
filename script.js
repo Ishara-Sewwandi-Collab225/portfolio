a36b674c8e9db1c9d33151af67297075f72a9477
Isha Karunarathna <isharas589@gmail.com>
2025-06-14 18:18:07 +0530

    commit

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ function closeMenu() {
 
 window.addEventListener('scroll', () => {
     if (scrollY > 50) {
-        navBar.classList.add( 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
-        navLinks.classList.remove( 'shadow-sm', 'bg-opacity-50');
+        navBar.classList.add( 'bg-white','bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
     } else {
-        navBar.classList.remove( 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
-        navLinks.classList.add('shadow-sm', 'bg-opacity-50');
+        navBar.classList.remove( 'bg-white','bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.add('bg-white','shadow-sm', 'bg-opacity-50');
     }
 })
 
